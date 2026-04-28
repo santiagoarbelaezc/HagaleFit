56293f3451b75f58e1fe3b899a14ee39d72474e2
Santiago Arbelaez Contreras <santiago.arbelaezc@uqvirtual.edu.co>
2026-04-28 01:58:58 -0500

    subiendo cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://hagalefit-env.eba-jewwdddw.us-east-2.elasticbeanstalk.com/api/fit-agent',
-  planWebhookUrl: 'https://hagalefit-env.eba-jewwdddw.us-east-2.elasticbeanstalk.com/api/fit-agent/generate-plan',
-  chatWebhookUrl: 'https://hagalefit-env.eba-jewwdddw.us-east-2.elasticbeanstalk.com/api/fit-agent/chat'
+  apiUrl: 'https://d21q5pjq1w5lra.cloudfront.net/api/fit-agent',
+  planWebhookUrl: 'https://d21q5pjq1w5lra.cloudfront.net/api/fit-agent/generate-plan',
+  chatWebhookUrl: 'https://d21q5pjq1w5lra.cloudfront.net/api/fit-agent/chat'
 };
