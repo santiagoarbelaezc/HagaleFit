afe6c0258344c79e1b59c84518e1968d4f446ab1
Santiago Arbelaez Contreras <santiago.arbelaezc@uqvirtual.edu.co>
2026-04-28 01:35:26 -0500

    fix: mixed content and timeout

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://hagalefit-env.eba-jewwdddw.us-east-2.elasticbeanstalk.com/api/fit-agent',
-  planWebhookUrl: 'http://hagalefit-env.eba-jewwdddw.us-east-2.elasticbeanstalk.com/api/fit-agent/generate-plan',
-  chatWebhookUrl: 'http://hagalefit-env.eba-jewwdddw.us-east-2.elasticbeanstalk.com/api/fit-agent/chat'
+  apiUrl: 'https://hagalefit-env.eba-jewwdddw.us-east-2.elasticbeanstalk.com/api/fit-agent',
+  planWebhookUrl: 'https://hagalefit-env.eba-jewwdddw.us-east-2.elasticbeanstalk.com/api/fit-agent/generate-plan',
+  chatWebhookUrl: 'https://hagalefit-env.eba-jewwdddw.us-east-2.elasticbeanstalk.com/api/fit-agent/chat'
 };
